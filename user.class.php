b18985c711e665e41f3436142d1adb127bd57204
Julian Stahnke <mail@julianstahnke.com>
2009-12-16 22:07:10 +0100

    initial import

---
+++ user.class.php
@@ -0,0 +1,146 @@
+<?php
+
+class User {
+    protected $name;
+    protected $expires = 3600;
+    
+    function User ($name)
+    {
+        $this->name = $name;
+    }
+    
+    public function __get($key)
+    {
+        if ($key == "name") return $this->name;
+    }
+    
+    public function getWeeklyChartList ()
+    {
+        $key = "user:".$this->name.":weeklychartlist";
+        $r = Store::get();
+        $list = $r->get($key);
+        
+        if (!$list) {
+            $args = array(
+                "method" => "user.getweeklychartlist",
+                "user" => $this->name,
+            );
+            $list = API::request($args);
+            if ($list) {
+                $r->set($key, $list);
+                $r->expire($key, $this->expires);
+            }
+        }
+        
+        if ($list) {
+            $list = json_decode($list);
+            return $list->weeklychartlist->chart;
+        } else {
+            return null;            
+        }
+    }
+    
+    public function getWeeklyArtistChartCount ($from = null, $to = null)
+    {
+        return $this->getWeeklyChartCount("artist", $from, $to);
+    }
+    
+    public function getWeeklyAlbumChartCount ($from = null, $to = null)
+    {
+        return $this->getWeeklyChartCount("album", $from, $to);
+    }
+    
+    public function getWeeklyTrackChartCount ($from = null, $to = null)
+    {
+        return $this->getWeeklyChartCount("track", $from, $to);
+    }
+    
+    public function getWeeklyChartCount ($type = "artist", $from = null, $to = null)
+    {
+        $r = Store::get();
+        if ($from && $to) {
+            $key = "user:".$this->name.":weeklychart:".$type.":".$from.":".$to.":count";
+        } else {
+            $key = "user:".$this->name.":weeklychart:".$type.":latest:count";
+        }
+        $count = $r->get($key);
+        if ($count) return $count;
+        
+        $chart = $this->getWeeklyChart($type, $from, $to);
+        
+        if ($chart) {
+            $count = 0;
+            foreach ($chart as $object => $item) {
+                $count += $item->playcount;
+            }
+            $r->set($key, $count);
+            if (!($from && $to)) $r->expire($key, $this->expires);
+            
+            return $count;
+        }
+        
+        return null;            
+    }
+    
+    public function getWeeklyArtistChart ($from = null, $to = null)
+    {
+        return $this->getWeeklyChart("artist", $from, $to);
+    }
+    
+    public function getWeeklyAlbumChart ($from = null, $to = null)
+    {
+        return $this->getWeeklyChart("album", $from, $to);
+    }
+    
+    public function getWeeklyTrackChart ($from = null, $to = null)
+    {
+        return $this->getWeeklyChart("track", $from, $to);
+    }
+    
+    public function getWeeklyChart ($type = "artist", $from = null, $to = null)
+    {
+        $r = Store::get();
+        if ($from && $to) {
+            $key = "user:".$this->name.":weeklychart:".$type.":".$from.":".$to;
+            $chart = $r->get($key);
+
+            if (!$chart) {
+                $args = array(
+                    "method" => "user.getweekly".$type."chart",
+                    "from" => $from,
+                    "to" => $to,
+                    "user" => $this->name,
+                );
+                $chart = API::request($args);
+                if ($chart) {
+                    $r->set($key, $chart);
+                }
+            }
+        } else {
+            $key = "user:".$this->name.":weeklychart:".$type.":latest";  
+            $chart = $r->get($key);
+            
+            if (!$chart) {
+                $args = array(
+                    "method" => "user.getweekly".$type."chart",
+                    "user" => $this->name,
+                );
+                $chart = API::request($args);
+                if ($chart) {
+                    $r->set($key, $chart);
+                    $r->expire($key, $this->expires);
+                }
+            }
+        }
+        
+        if ($chart) {
+            $chart = json_decode($chart);
+            $var = "weekly".$type."chart";
+            return $chart->$var->$type;
+        } else {
+            return null;            
+        }
+    }
+}
+
+?>
---
+++ yearly_chart.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once("blah.php");
+require_once("user.class.php");
+
+date_default_timezone_set("Europe/Berlin");
+setlocale(LC_ALL, 'de_DE');
+ini_set('display_errors', 1);
+error_reporting(E_ERROR);
+
+$username = $_GET["user"] ? $_GET["user"] : "julians";
+$chartyear = intval($_GET["year"]) ? $_GET["year"] : 2009;
+$user = new User($username);
+$list = $user->getWeeklyChartList();
+
+$artists = array();
+$max = 0;
+
+$years = array();
+for ($i=0; $i < count($list); $i++) {
+    $year = strftime("%Y", $list[$i]->to);
+    $years[$year] = true;
+    $year = intval($year);
+    if ($year == $chartyear) {
+        $chart = $user->getWeeklyArtistChart($list[$i]->from, $list[$i]->to);
+        if ($chart && count($chart) > 1) {
+            for ($j=0; $j < count($chart); $j++) { 
+                $artist = $chart[$j];
+                if ($artists[$artist->name]) {
+                    $artists[$artist->name] += $artist->playcount;
+                } else {
+                    $artists[$artist->name] = $artist->playcount;
+                }
+            }
+        } else if ($chart) {
+            if ($artists[$chart->name]) {
+                $artists[$chart->name] += $chart->playcount;
+            } else {
+                $artists[$chart->name] = $chart->playcount;
+            }
+        }
+    }
+}
+
+arsort($artists);
+$max = null;
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Last.fm Yearly Charts</title>
+	<link rel="stylesheet" href="css/yearly.css">
+	
+</head>
+<body>
+
+    <form action="" method="get">
+        <p>
+            <label for="username">Last.fm username</label>
+            <br>
+            <input type="text" name="username" value="<?php echo $username; ?>" placeholder="Your username" id="username">
+        </p>
+        <?php
+            if (count($years)) {
+                echo "<p>";
+                echo '<label for="year">Year</label>';
+                echo '<br>';
+                echo '<select name="year" id="year">';
+                foreach ($years as $key => $value) {
+                    echo '<option value="'.$key.'"';
+
+                    if (intval($key) == $chartyear) echo ' selected';
+                    echo '>'.$key.'</option>';
+                }
+                echo '</select>';
+                echo '</p>';
+            }
+        ?>
+        <input type="submit" name="gogogo" value="Make it so!" id="gogogo">
+    </form>
+
+    <ul>
+        <?php
+            foreach ($artists as $key => $value) {
+                if (!$max) $max = $value;
+                echo "<li>";
+                print("<span class='playcount'>" . $value . "</span>");
+                print("<span class='artist'>" . $key . "</span>");
+                print("<div class='chartbar' style='width: ".(round($value/$max, 2)*100)."%'> </div>");
+                echo "</li>";
+            }
+        ?>
+    </ul>
+
+</body>
+</html>
